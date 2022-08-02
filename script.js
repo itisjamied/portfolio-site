cd8d25bb58311563363a1f01204d65f3fcda8fc9
Jamie De Los Santos <77027304+itisjamied@users.noreply.github.com>
2022-08-02 12:13:30 -0400

    made timeline responsive + move pictures

--- script.js
+++ script.js
@@ -1,45 +1,55 @@
-let sliderContainer = document.querySelector('.slider-container');
-let innerSlider = document.querySelector('.projects');
+// let sliderContainer = document.querySelector('.slider-container');
+// let innerSlider = document.querySelector('.projects');
 
-let pressed = false;
-let startX;
-let x;
+// let pressed = false;
+// let startX;
+// let x;
 
-sliderContainer.addEventListener("mousedown", (e) => {
-    pressed = true;
-    startX = e.offsetX - innerSlider.offsetLeft;
-    sliderContainer.style.cursor = "grabbing";
-});
+// sliderContainer.addEventListener("mousedown", (e) => {
+//     pressed = true;
+//     startX = e.offsetX - innerSlider.offsetLeft;
+//     sliderContainer.style.cursor = "grabbing";
+// });
 
 
-sliderContainer.addEventListener("mouseenter", () => {
-    // sliderContainer.style.cursor = "grab";
-});
+// sliderContainer.addEventListener("mouseenter", () => {
+//     // sliderContainer.style.cursor = "grab";
+// });
 
-sliderContainer.addEventListener("mouseup", () => {
-    sliderContainer.style.cursor = "grab";
-    pressed = false;
-});
+// sliderContainer.addEventListener("mouseup", () => {
+//     sliderContainer.style.cursor = "grab";
+//     pressed = false;
+// });
 
-sliderContainer.addEventListener("mousemove", (e) => {
-    if (!pressed) return;
-    e.preventDefault();
+// sliderContainer.addEventListener("mousemove", (e) => {
+//     if (!pressed) return;
+//     e.preventDefault();
 
-    x = e.offsetX;
+//     x = e.offsetX;
 
-    innerSlider.style.left = `${x - startX}px`;
-    checkBoundary();
-});
+//     innerSlider.style.left = `${x - startX}px`;
+//     checkBoundary();
+// });
 
-const checkBoundary = () => {
-    let outer = sliderContainer.getBoundingClientRect();
-    let inner = innerSlider.getBoundingClientRect();
+// const checkBoundary = () => {
+//     let outer = sliderContainer.getBoundingClientRect();
+//     let inner = innerSlider.getBoundingClientRect();
 
-    if (parseInt(innerSlider.style.left) > 0) {
-        innerSlider.style.left = "0px";
+//     if (parseInt(innerSlider.style.left) > 0) {
+//         innerSlider.style.left = "0px";
+//     }
+
+//     if (inner.right < outer.right) {
+//         innerSlider.style.left = `-${inner.width - outer.width}px`;
+//     }
+// };
+
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
     }
-
-    if (inner.right < outer.right) {
-        innerSlider.style.left = `-${inner.width - outer.width}px`;
-    }
-};
+}
